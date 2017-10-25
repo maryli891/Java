6408e36424dcd5bc03a992e44962cf1712f6cc8f
maryli891 <marytta095@gmail.com>
2017-10-24 21:09:36 -0300

    Inicio Proyecto

---
+++ src/proyecto1mariane/Proyecto1Mariane.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1mariane;
+
+/**
+ *
+ * @author LC22
+ */
+public class Proyecto1Mariane {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo,soy Mariane!!");
+    }
+    
+}
